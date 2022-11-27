192dae60368ea3e57d51191de9045f372bc9bf04
haiso-yen <58282037abc123@gmail.com>
2022-11-27 18:46:49 +0800

    Revert "Merge remote-tracking branch 'upstream/main'"
    This reverts commit a161d4e1e5c6720e42b652498c1f9309b9a49ff8, reversing
    changes made to 32f9f4c253150787cd48a37733aeb293380fd350.

--- HazepolisNew/Assets/Scripts/V1CamController.cs
+++ HazepolisNew/Assets/Scripts/V1CamController.cs
@@ -22,18 +22,4 @@ public class V1CamController : MonoBehaviour
         }
         DontDestroyOnLoad(gameObject);
     }
-
-    private Cinemachine.CinemachineConfiner confiner;
-    private Collider2D mapConfiner;
-
-    private void Start()
-    {
-        confiner = GetComponent<Cinemachine.CinemachineConfiner>();
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        mapConfiner = GameObject.FindWithTag("MapConfiner").GetComponent<Collider2D>();
-        confiner.m_BoundingShape2D = mapConfiner;
-    }
 }
