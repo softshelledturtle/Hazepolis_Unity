422705c94d2edb6e19ecc799ea6bd6d14b1ce673
Karen9041 <71089781+Karen9041@users.noreply.github.com>
2022-11-28 23:51:00 +0800

    整理

--- HazepolisNew/Assets/Scripts/Questing/Quest.cs
+++ HazepolisNew/Assets/Scripts/Questing/Quest.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Quest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<Goal> Goals { get; set; } = new List<Goal>();
+    public string QuestName { get; set; }
+    public string Description { get; set; }
+    public Item ItemReward { get; set; }
+    public bool Completed { get; set; }
+
+    public void CheckGoals()
     {
-        
+        Completed = Goals.All(g => g.Completed);
+        if (Completed) GiveREward();
     }
 
-    // Update is called once per frame
-    void Update()
+    void GiveREward()
     {
-        
+        if(ItemReward != null)
+        {
+            
+        }
     }
 }
