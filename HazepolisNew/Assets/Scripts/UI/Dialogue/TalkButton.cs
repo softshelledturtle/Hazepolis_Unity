87c4bbe030e2c6ece780da60134020bbb24cee07
haiso-yen <58282037abc123@gmail.com>
2022-11-28 22:30:58 +0800

    對話系統改版
    可用文本管理
    +打字機效果
    一部分文本還沒裝

--- HazepolisNew/Assets/Scripts/UI/Dialogue/TalkButton.cs
+++ HazepolisNew/Assets/Scripts/UI/Dialogue/TalkButton.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class TalkButton : MonoBehaviour
 {
     public GameObject Button;
@@ -12,13 +13,13 @@ public class TalkButton : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         Button.SetActive(true);
-        DialogUI.SetActive(true);
+        //DialogUI.SetActive(true);
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
         Button.SetActive(false);
-        DialogUI.SetActive(false);
+        //DialogUI.SetActive(false);
     }
 
     void Update()
@@ -26,7 +27,7 @@ public class TalkButton : MonoBehaviour
         if (Button.activeSelf && Input.GetKeyDown(KeyCode.T))
         {
             DialogUI.SetActive(true);
-            trigger.StartDialog();
+            //trigger.StartDialog();
             Debug.Log("Conversation");
             if (this.tag == "Interactable")
             {
