93ec6b2e2a0e2ae9e1da3d9173345e245b20eed1
Karen9041 <71089781+Karen9041@users.noreply.github.com>
2023-04-07 10:32:29 +0800

    Revert "Squashed commit of the following:"
    This reverts commit 6ac12c78dcd8004b43d5fd4eafcdfd503c9241be.

--- HazepolisNew/Assets/Scripts/CharacterController2D.cs
+++ HazepolisNew/Assets/Scripts/CharacterController2D.cs
@@ -9,8 +9,7 @@ public class CharacterController2D : MonoBehaviour
 
 	public GameObject[] MainPlayer;
 	private GameObject currentPlayer, previousPlayer;
-	public bool foundDan = false;
-	public float threshold = 0.1f; // 滾輪速度閾值
+	public bool foundDan = false; 
 
 	[SerializeField] private int iCharcaterCount = 0;
 	public Camera cam;
@@ -72,82 +71,56 @@ public class CharacterController2D : MonoBehaviour
 
 	void MagicDetect()
 	{
-		int layerMask = 1 << LayerMask.NameToLayer("Interactive");
-		var worldPoint = GetMouseWorldPoint();
-		RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero, Mathf.Infinity, layerMask);
-        //DegugDraw3Axis(worldPoint);
-
-		if (Input.GetKey(KeyCode.E) && hit.collider != null)
-		{
-			String activeObj = hit.transform.name;
-			GameObject hitObj = GameObject.Find(activeObj);
-			//Debug.Log("hit.collider.name:" + hit.collider.name);
-
-			if (iCharcaterCount == 0)
-			{
-                if (hitObj.GetComponent<ObjectController>().isFlowable)
-                {
-					//TimeControl
-					float scroll = Input.GetAxis("Mouse ScrollWheel");
-
-					if (Mathf.Abs(scroll) >= threshold)
-					{
-						Debug.Log(scroll);
-						currentPlayer.gameObject.SendMessage("MagicTimeReverseSpeedUp"); //跑動畫
-						hitObj.SendMessage("ControlTime", scroll); //傳至ObjectController
-					}
-
-					//TimePause
-					if (Input.GetMouseButtonDown(1))
-					{
-						Debug.Log("hit.collider.name:" + hit.collider.name + "PauseTime");
-						currentPlayer.gameObject.SendMessage("MagicTimeStop"); //跑動畫
-						hitObj.SendMessage("PauseTime"); //傳至ObjectController
-					}
-				}
-			}
-
-			if (iCharcaterCount == 1)
-			{
-				if (hitObj.GetComponent<ObjectController>().isDragable)
-				{
-					if (Input.GetMouseButtonDown(0)) //跑動畫
-					{
-						currentPlayer.gameObject.SendMessage("Magic");
-						Debug.Log("hit.collider.name:" + hit.collider.name + "Drag");
-					}
-					if (Input.GetMouseButton(0))
-					{
-						StartCoroutine(Drag(hit.collider));
-					}
-				}
-			}
-		}
+        Vector3 mousePos = Input.mousePosition;
+		mousePos.z = -1.35f;
 		
-	}
+		Vector3 objectPos = cam.WorldToScreenPoint(transform.position);
+		mousePos.x = mousePos.x - objectPos.x;
+		mousePos.y = mousePos.y - objectPos.y;
 
-	Vector3 GetMouseWorldPoint()
-	{
-		var mousePosition = Input.mousePosition;
-		mousePosition.z = -cam.transform.position.z;
-		var worldPoint = cam.ScreenToWorldPoint(mousePosition);
-		return worldPoint;
-	}
-	IEnumerator Drag(Collider2D collider)
-	{
-		var obj = collider.gameObject;
-		var dragPoint = obj.transform.position;
-		var beginMousePoint = GetMouseWorldPoint();
+		float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
 
-		while (Input.GetMouseButton(0))
-		{
-			var worldPoint = GetMouseWorldPoint();
-			obj.transform.position = dragPoint - beginMousePoint + worldPoint;
+		RaycastHit2D objHit = Physics2D.Raycast(transform.position, transform.right, 10, 1 << LayerMask.NameToLayer("Interactive"));
+		Debug.DrawRay(transform.position, transform.right * 10, Color.red);
 
-			yield return null;
-		}
+		if (Input.GetKey(KeyCode.E) && objHit.collider != null)
+        {
+			String activeObj = objHit.transform.name;
+			Debug.DrawRay(transform.position, transform.right * 10, Color.red);
+
+			if(iCharcaterCount == 0)
+            {
+				//TimeControl
+				if (Input.mouseScrollDelta.y != 0)
+				{
+					currentPlayer.gameObject.SendMessage("MagicTimeReverseSpeedUp");
+					float mouseScr = Input.mouseScrollDelta.y;
+					Debug.Log(activeObj);
+					GameObject.Find(activeObj).SendMessage("ControlTime", mouseScr);
+				}
+				//TimePause
+				if (Input.GetMouseButtonDown(1))
+				{
+					currentPlayer.gameObject.SendMessage("MagicTimeStop");
+					GameObject.Find(activeObj).SendMessage("PauseTime");
+				}
+			}
+
+			if(iCharcaterCount == 1)
+            {
+				if (Input.GetMouseButtonDown(0))
+				{
+					currentPlayer.gameObject.SendMessage("Magic");
+					//GameObject.Find(activeObj).SendMessage("ObjectDrag");
+				}
+				if (Input.GetMouseButton(0))
+				{
+					GameObject.Find(activeObj).SendMessage("ObjectDrag");
+				}
+			}	
+		}	
 	}
-	
 }
 
 
--- HazepolisNew/Assets/Scripts/ObjectController.cs
+++ HazepolisNew/Assets/Scripts/ObjectController.cs
@@ -4,51 +4,54 @@ using UnityEngine;
 public class ObjectController : MonoBehaviour
 {
     private Animator animator;
-    public bool isDragable;
-    public bool isFlowable;
-    public bool isChangDirection;
+    //private float magicState;
 
-    public float directionMultiplier = 1.0f;
-    public float predirection = 0f;
-    public float speedMultiplier = 1f;// ³t«×­¿²v
+    // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
-        animator.speed = 0;
-        animator.SetFloat("Direction", 0);
+        animator.SetFloat("speed", 0);
     }
 
-    public void ControlTime(float scroll)
+    // Update is called once per frame
+    void Update()
     {
-        float dragAmount = Mathf.Abs(scroll);
-        if (dragAmount !=0)
+
+    }
+
+    public void ControlTime(float timer)
+    {
+        float Pretimer = 0;
+
+        if (timer > 0 && Pretimer!=1)
         {
-            float direction = Mathf.Sign(scroll) * directionMultiplier;
-            float speed = dragAmount * speedMultiplier;
-
-            animator.speed += speed;
-
-            if (direction != predirection)
-            {
-                isChangDirection = true;
-                predirection = direction;
-            }
-            else
-            {
-                isChangDirection = false;
-            }
-            if (isChangDirection)
-            {
-                animator.speed = speed;
-                animator.SetFloat("Direction", direction);
-                Debug.Log(this.name+direction);
-            }
+            animator.SetFloat("speed", 1);
+            Pretimer = 1;
         }
+
+        if (timer < 0 && Pretimer != -1)
+        {
+            animator.SetFloat("speed", -1);
+            Pretimer = -1;
+        }
+        //Debug.Log("ControlTime" + timer);
+        //animator.SetFloat("speed", timer);
+        //magicState = timer;
     }
     public void PauseTime()
     {
         Debug.Log("PauseTime");
-        animator.speed = 0;
+        animator.SetFloat("speed", 0);
         //magicState = 0;
+    }
+
+    public void ObjectDrag()
+    {
+        Debug.Log("ObjectDrag");
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        objPosition.z = gameObject.transform.position.z;
+        objPosition.x = gameObject.transform.position.x;
+        transform.position = objPosition; 
     }
 }
