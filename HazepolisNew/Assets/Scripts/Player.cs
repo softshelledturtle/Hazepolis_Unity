62fccb3be207a6004095732f38e501c6cfd8fbb9
haiso-yen <58282037abc123@gmail.com>
2023-04-09 21:36:27 +0800

    Hp條安裝 -按K可以扣血(滿血20，扣一次1) -script動HealthBar&player -扣血寫在player裡

--- HazepolisNew/Assets/Scripts/Player.cs
+++ HazepolisNew/Assets/Scripts/Player.cs
@@ -6,8 +6,30 @@ public class Player : MonoBehaviour
 {
     public CharacterStat characterStats;
 
+    public int MaxHealth = 20;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
     private void Start()
     {
         characterStats = new CharacterStat(10);
+
+        currentHealth = MaxHealth;
+        healthBar.SetMaxHealth(MaxHealth);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K)) {
+            TakeDamage(1);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        healthBar.SetHealth(currentHealth);
     }
 }
